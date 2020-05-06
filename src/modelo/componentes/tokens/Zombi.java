36b243b6249e35ee63d98f7ec11ba3d2093d5988
Felipe <fesanmar@gmail.com>
2020-05-07 00:15:32 +0200

    Implementa constructor, die y setCasilla de zombie

--- src/modelo/componentes/tokens/Zombi.java
+++ src/modelo/componentes/tokens/Zombi.java
@@ -2,21 +2,33 @@ package modelo.componentes.tokens;
 
 import modelo.componentes.Componente;
 import modelo.componentes.tablero.Casilla;
+import modelo.partida.Partida;
 
-public class Zombi extends Componente implements Token {
+public class Zombi extends Componente implements Token 
+{
+	Partida partida;
+	Casilla casilla;
 
-	public Casilla getCasilla() {
-		// TODO - implement Zombi.getCasilla
-		throw new UnsupportedOperationException();
+	public Zombi(Partida partida)
+	{
+		super(SIDE, "./images/zombi.jpeg");
+		this.partida = partida;
+	}
+	
+	public Casilla getCasilla() 
+	{
+		return casilla;
 	}
 
 	/**
 	 * 
 	 * @param casilla
 	 */
-	public void setCasilla(Casilla casilla) {
-		// TODO - implement Zombi.setCasilla
-		throw new UnsupportedOperationException();
+	public void setCasilla(Casilla casilla) 
+	{
+		setX(casilla.getX());
+		setY(casilla.getY());
+		this.casilla = casilla;
 	}
 
 	public void move() {
@@ -29,9 +41,10 @@ public class Zombi extends Componente implements Token {
 		throw new UnsupportedOperationException();
 	}
 
-	public void die() {
-		// TODO - implement Zombi.die
-		throw new UnsupportedOperationException();
+	public void die() 
+	{
+		partida.discard(this);
+		
 	}
 
 }
