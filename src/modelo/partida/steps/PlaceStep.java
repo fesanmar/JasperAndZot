13f60ef7e43e208933a0a9539352035006bdf116
Felipe <fesanmar@gmail.com>
2020-05-07 13:07:59 +0200

    Termina de implementar PlaceStep

--- src/modelo/partida/steps/PlaceStep.java
+++ src/modelo/partida/steps/PlaceStep.java
@@ -5,61 +5,61 @@ import modelo.componentes.tokens.Token;
 import modelo.partida.Partida;
 import modelo.partida.assaults.NormalAssault;
 
-public class PlaceStep implements Step, Runnable {
+public class PlaceStep implements Step, Runnable
+{
 
 	private Partida partida;
 	private Thread thread;
-	private boolean dicesRolled = false;
 
 	public PlaceStep(Partida partida)
 	{
 		this.partida = partida;
 		thread = new Thread(this, "PlaceStep thread");
 	}
-	
-	public void descend() 
+
+	public void descend()
 	{
 		partida.getManydice().roll();
 		partida.getAppearDice().roll();
-		dicesRolled = true;
 		partida.repaintDiceArea();
-		
-		Token[] drawedTokens 
-			= new Token[partida.getAssault().getNumberOfTokensToDraw()];
+
+		Token[] drawedTokens = new Token[partida.getAssault().getNumberOfTokensToDraw()];
 		for (int i = 0; i < drawedTokens.length; i++)
 		{
 			try
 			{
 				drawedTokens[i] = partida.getTokensBag().getToken();
-			} 
-			catch (EmptyBagException ebe)
+			} catch (EmptyBagException ebe)
 			{
 				if (partida.getAssault() instanceof NormalAssault)
 				{
 					partida.getTokensBag().refillBag();
 					partida.setAssault(partida.getAggravatedAssault());
-				}
-				else
+				} else
 				{
 					partida.end();
 				}
 			}
 		}
-		
+
 		partida.getAssault().placeTokens(drawedTokens);
 		partida.repaintHome();
+		partida.setStep(partida.getMoveShootStep());
+		partida.getStep().display();
 	}
 
-	public void place(int x, int y) 
+	public void place(int x, int y)
 	{
 	}
 
-	public void moveAndShoot() {
+	public void moveAndShoot()
+	{
 		// TODO - implement PlaceStep.moveAndShoot
 		throw new UnsupportedOperationException();
 	}
 
-	public void Smash() {
+	public void Smash()
+	{
 		// TODO - implement PlaceStep.Smash
 		throw new UnsupportedOperationException();
 	}
@@ -75,26 +75,24 @@ public class PlaceStep implements Step, Runnable {
 	@Override
 	public boolean isSelectable(int x, int y)
 	{
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void run()
 	{
-		if (dicesRolled == false)
+
+		try
 		{
-			try
-			{
-				Thread.sleep(3000);
-				partida.getTablero().setMessage("");
-				partida.repaintMessageArea();
-				descend();
-			} catch (InterruptedException e)
-			{
-				e.printStackTrace();
-			}
+			Thread.sleep(3000);
+			partida.getTablero().setMessage("");
+			partida.repaintMessageArea();
+			descend();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
 		}
+
 	}
 
 }
