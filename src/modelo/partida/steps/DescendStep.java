226446be57ab814d253c35b6757ec61dcc6d552b
Felipe <fesanmar@gmail.com>
2020-05-11 19:31:18 +0200

    Los 2x desaparecen al terminar junto a una calabaza o a la barrera

--- src/modelo/partida/steps/DescendStep.java
+++ src/modelo/partida/steps/DescendStep.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import modelo.componentes.tablero.Tablero;
 import modelo.componentes.tokens.Bomba;
 import modelo.componentes.tokens.Flores;
+import modelo.componentes.tokens.Multiplicador;
 import modelo.componentes.tokens.Token;
 import modelo.partida.Partida;
 
@@ -40,7 +41,8 @@ public class DescendStep implements Step, Runnable {
 			}
 		}
 		partida.getTokens().removeAll(flowersToBurn);
-		// Explotan las bombas que quedan frente a calabazas
+		// Explotan las bombas que quedan frente a calabazas o las que llegan a la 
+		// barrera.  Y hace desparecer los x2 en la misma situación
 		ArrayList<Token> tokensLeft =partida.getTokens(); 
 		for (int i = 0; i < tokensLeft.size(); i++)
 		{
@@ -50,7 +52,15 @@ public class DescendStep implements Step, Runnable {
 						|| tokensLeft.get(i).getCasilla().getRow() == Tablero.PUMPKIN_ROW)
 				{
 					tokensLeft.get(i).atack();
-				}				
+				}
+			}
+			else if (tokensLeft.get(i) instanceof Multiplicador)
+			{
+				if (tokensLeft.get(i).getCasilla().getBottomCasilla().hasPumpkin() 
+						|| tokensLeft.get(i).getCasilla().getRow() == Tablero.PUMPKIN_ROW)
+				{
+					tokensLeft.get(i).die();
+				}
 			}
 		}
 		partida.repaintHome();
